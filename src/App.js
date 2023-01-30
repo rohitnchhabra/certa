a67da8f47596d5f2b03202b48a1030d71ae61aad
rohitnchhabra <49113896+rohitnchhabra@users.noreply.github.com>
2023-01-30 23:56:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [counter, setCounter] = React.useState(0);
+  const [showReset, setShowReset] = React.useState(false);
+  const [intervalId, setIntervalId] = React.useState(0);
+
+
+  const handleStart = () => {
+    const newIntervalId = setInterval(()=> {
+      setCounter(prevCount => prevCount + 1);
+    }, 1000);
+    setIntervalId(newIntervalId);
+  };
+
+  const handlePause = () => {
+    clearInterval(intervalId);
+    setIntervalId(0);
+    setShowReset(true);
+  }
+
+  const handleReset = () => {
+    setShowReset(false);
+    setCounter(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{counter}</div>
+      {!intervalId ? !showReset && <button onClick={handleStart}>Start</button> : null}
+      {intervalId ? !showReset && <button onClick={handlePause}>Pause</button> : null}
+      {showReset && <button onClick={handleReset}>Reset</button>}
+
     </div>
   );
 }
